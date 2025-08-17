0858ba9482c5360a6268b77b90545b7611f7a3ef
lunarXhvb <lunarxhub@gmail.com>
2025-08-17 10:08:05 -0500

    Update Games.lua

--- Lunar-Hub/Games.lua
+++ Lunar-Hub/Games.lua
@@ -1,1 +1,12 @@
+local Games = {
+  [3623096087] = "", -- Muscle Legends [ LUNAR UI ]
+  [3623096087] = "", -- Muscle Legends [ FLUENT ]
+  [3101667897] = "", -- Legends Of Speed
+  [126884695634066] = "", -- Grow A Garden
+  [2753915549] = "", -- Blox Fruits
+  [537413528] = "", -- Build a Boat For Treasure
+  [3956818381] = "", -- Ninja Legends 1
+  [5977280685] = "", -- Ninja Legends 2
+}
 
+return Games
