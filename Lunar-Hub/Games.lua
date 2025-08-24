2d8e420a384953f005252fb9b27a05ec3f5e5992
lunarXhvb <lunarxhub@gmail.com>
2025-08-23 22:58:08 -0500

    Update Games.lua

--- Lunar-Hub/Games.lua
+++ Lunar-Hub/Games.lua
@@ -1,11 +1,11 @@
 local Games = {
   [3623096087] = "", -- Muscle Legends [ LUNAR UI ]
-  [3623096087] = "https://raw.githubusercontent.com/lunarXhvb/LunarXHub/refs/heads/main/Games/Games/Files/MuscleLegends.luau.txt", -- Muscle Legends [ FLUENT ]
+  [3623096087] = "https://raw.githubusercontent.com/lunarXhvb/LunarXHub/refs/heads/main/Games/Games/Files/MuscleLegends.luau", -- Muscle Legends [ FLUENT ]
   [3101667897] = "", -- Legends Of Speed
   [126884695634066] = "", -- Grow A Garden
-  [2753915549] = "https://raw.githubusercontent.com/lunarXhvb/LunarXHub/refs/heads/main/Games/Games/Files/Blox-Fruits.luau.txt", -- Blox Fruits
-  [4442272183] = "https://raw.githubusercontent.com/lunarXhvb/LunarXHub/refs/heads/main/Games/Games/Files/Blox-Fruits.luau.txt", -- Blox Fruits
-  [7449423635] = "https://raw.githubusercontent.com/lunarXhvb/LunarXHub/refs/heads/main/Games/Games/Files/Blox-Fruits.luau.txt", -- Blox Fruits
+  [2753915549] = "https://raw.githubusercontent.com/lunarXhvb/LunarXHub/refs/heads/main/Games/Games/Files/Blox-Fruits.luau", -- Blox Fruits
+  [4442272183] = "https://raw.githubusercontent.com/lunarXhvb/LunarXHub/refs/heads/main/Games/Games/Files/Blox-Fruits.luau", -- Blox Fruits
+  [7449423635] = "https://raw.githubusercontent.com/lunarXhvb/LunarXHub/refs/heads/main/Games/Games/Files/Blox-Fruits.luau", -- Blox Fruits
   [537413528] = "", -- Build a Boat For Treasure
   [3956818381] = "", -- Ninja Legends 1
   [5977280685] = "", -- Ninja Legends 2
