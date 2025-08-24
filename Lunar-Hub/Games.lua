f340b02fe9675c76303c747f740b16291cf9723b
lunarXhvb <lunarxhub@gmail.com>
2025-08-24 14:51:00 -0500

    Update and rename Games.luaa to Games.lua

---
+++ Lunar-Hub/Games.lua
@@ -0,0 +1,14 @@
+local Games = {
+  [3623096087] = "", -- Muscle Legends [ LUNAR UI ]
+  [3623096087] = "https://raw.githubusercontent.com/lunarXhvb/LunarXHub/refs/heads/main/Games/Games/Files/MuscleLegends.luau", -- Muscle Legends [ FLUENT ]
+  [3101667897] = "", -- Legends Of Speed
+  [126884695634066] = "", -- Grow A Garden
+  [2753915549] = "https://github.com/lunarXhvb/Lunar_Hub/raw/refs/heads/main/Games/Blox%20Fruits", -- Blox Fruits
+  [4442272183] = "https://github.com/lunarXhvb/Lunar_Hub/raw/refs/heads/main/Games/Blox%20Fruits", -- Blox Fruits
+  [7449423635] = "https://github.com/lunarXhvb/Lunar_Hub/raw/refs/heads/main/Games/Blox%20Fruits", -- Blox Fruits
+  [537413528] = "", -- Build a Boat For Treasure
+  [3956818381] = "", -- Ninja Legends 1
+  [5977280685] = "", -- Ninja Legends 2
+}
+
+return Games
